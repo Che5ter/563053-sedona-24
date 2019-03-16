775bc3083d8f30f8460690ac8421bcd1b0789b28
CHESTER <hatake18@bk.ru>
2019-03-16 19:08:27 +0300

    критерии + минимайзы

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var link = document.querySelector(".searching-hotel-link");
+var popup = document.querySelector(".modal-form");
+var form = popup.querySelector("form");
+var dateIn = popup.querySelector("[name=date-in]");
+var dateOut = popup.querySelector("[name=date-out]");
+var children = popup.querySelector("[name=children]");
+var adults = popup.querySelector("[name=adults]");
+
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("searching-hotel-show");
+  popup.classList.remove("searching-hotel-error");
+})
+form.addEventListener("submit", function (evt) {
+  if (!dateIn.value || !dateOut.value) {
+    evt.preventDefault();
+    popup.classList.remove("searching-hotel-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("searching-hotel-error");
+  }
+})
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("searching-hotel-show")) {
+      popup.classList.remove("searching-hotel-show");
+      popup.classList.remove("searching-hotel-error");
+    }
+  }
+})
